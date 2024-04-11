3f71a89281616678ce3b0e18d3898ddc24e6d874
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-04-11 12:38:40 -0700

    Search for a node value in BST

---
+++ 113-bst_search.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - Searches for a value in a BST
+ * @tree: Ptr to the root node of the to search.
+ * @value: Value to search for in the BST.
+ * Retrun: NUll if the tree is empty or the value is not found or else
+ * return ptr to the node containing the value
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree != NULL)
+	{
+		if (tree->n == value)
+			return ((bst_t *)tree);
+		if (tree->n > value)
+			return (bst_search(tree->left, value));
+		return (bst_search(tree->right, value));
+	}
+	return (NULL);
+}
